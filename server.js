2e53357319d42b9a0ff1ce87131da63646b6e4bf
nikitha_rajs <nikitharajhpk@gmail.com>
2024-10-28 21:06:49 +0530

    express setup

--- server.js
+++ server.js
@@ -1,21 +1,15 @@
-// fileName : server.js 
-// Example using the http module
-const http = require('http');
+const express = require('express');
 
-// Create an HTTP server
-const server = http.createServer((req, res) => {
-    // Set the response headers
-    res.writeHead(200, { 'Content-Type': 'text/html' });
 
-    // Write the response content
-    res.write('<h1>Hello, Node.js HTTP Server!</h1>');
-    res.end();
+// Next initialize the application
+const app = express();
+
+// routing path
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-// Specify the port to listen on
-const port = 3000;
-
 // Start the server
-server.listen(port, () => {
-    console.log(`Node.js HTTP server is running on port ${port}`);
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
 });
